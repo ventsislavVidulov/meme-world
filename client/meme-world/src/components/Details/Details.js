bd83cd6e3872c0e85217499c0d0d1aad9ab282b7
xcen3k <xcen3k@gmail.com>
2023-04-06 21:37:52 +0300

    add details view and some content

---
+++ client/meme-world/src/components/Details/Details.js
@@ -0,0 +1,13 @@
+import styles from './Details.module.css'
+
+const Details = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.card}>
+                <img className={styles.image} src="/1.jpg" />
+            </div>
+        </div>
+    )
+}
+
+export default Details;
---
+++ client/meme-world/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <button>
+                Log in
+            </button>
+            <button>
+                Register
+            </button>
+            <button>
+                Log out
+            </button>
+            <button>
+                My memes
+            </button>
+        </header>
+    )
+}
+
+export default Header;
