5fba991efeacbe4ed4a885010db25fa40d459c04
xcen3k <xcen3k@gmail.com>
2023-04-07 22:24:01 +0300

    Create authService.js

---
+++ client/src/services/authService.js
@@ -0,0 +1,24 @@
+import * as request from "./requester";
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const login = (email, password) => 
+    request.post(`${baseUrl}/login`, { email, password });
+
+
+export const logout = async (accessToken) => {
+    try {
+        const response = await fetch(`${baseUrl}/logout`, {
+            headers: {
+                'X-Authorization': accessToken
+            }
+        });
+
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const register = (email, password, fname, lname) =>
+    request.post(`${baseUrl}/register`, {email, password, fname, lname});
