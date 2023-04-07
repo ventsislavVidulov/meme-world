1a4a002aa893676f9b964133b288cd3202d52a1c
xcen3k <xcen3k@gmail.com>
2023-04-07 22:23:29 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 
 import Header from './components/Header/Header';
-import Details from './components/Details/Details';
+// import Details from './components/Details/Details';
+import { AuthProvider } from './contexts/authContext';
+import Login from './components/Login/Login';
+import Register from './components/Register/register';
+
+
 
 function App() {
   return (
-    <><Header />
-    <Details />
-    </>
+    <AuthProvider>
+      <Header />
+      <Register />
+      <Login />
+    </AuthProvider >
   );
 }
 
