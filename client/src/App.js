db7d9e5d5d80364b61ec09684c987e2da9d325ca
xcen3k <xcen3k@gmail.com>
2023-04-10 23:05:20 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 // import Details from './components/Details/Details';
 import { AuthProvider } from './contexts/authContext';
 import Login from './components/Login/Login';
 import Register from './components/Register/register';
+import Logout from './components/Logout/Logout';
+import Home from './components/Home/Home';
 
 
 
@@ -12,8 +16,12 @@ function App() {
   return (
     <AuthProvider>
       <Header />
-      <Register />
-      <Login />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/logout" element={<Logout/>}/>
+      </Routes>
     </AuthProvider >
   );
 }
