dfa313fca80a4d542ff3c1cd72806f4356d26ef6
xcen3k <xcen3k@gmail.com>
2023-04-13 17:22:57 +0300

    Create CreateMeme.js

---
+++ client/src/components/CreateMeme/CreateMeme.js
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router';
+
+import styles from './CreateMeme.module.css';
+
+import { createMeme } from '../../services/memeService';
+
+const CreateMeme = () => {
+
+    const navigate = useNavigate();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const memeData = Object.fromEntries(new FormData(e.target));
+        createMeme(memeData);
+
+        navigate('/');
+    }
+
+    return (
+        <div className={styles.container}>
+            <h1>CREATE MEME</h1>
+            <form onSubmit={onSubmit}>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="title">Title</label>
+                    <input className={styles.rounded} type="text" name="title" id="title" />
+                </div>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="imgeUrl">Image url</label>
+                    <input className={styles.rounded} type="text" name="imgUrl" id="imgeUrl" />
+                </div>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="description">Description</label>
+                    <input className={styles.rounded} type="text-area" name="description" id="description" />
+                </div>
+                <button type="submit" className={styles.rounded & styles.margin}>
+                    CREATE MEME
+                </button>
+            </form>
+        </div>
+
+    );
+}
+
+export default CreateMeme;
