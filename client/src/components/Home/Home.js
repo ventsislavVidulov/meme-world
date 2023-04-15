24ff24851fb3355982a1385561b3696c9294bf36
xcen3k <xcen3k@gmail.com>
2023-04-15 18:30:39 +0300

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,21 @@
+import styles from './Home.module.css'
+
 import { MemeCard } from "../MemeCard/MemeCard";
 
-const Home = () => { 
+const Home = ({ memes }) => {
 
     return (
-        <h1>
-            HOME
-        </h1>
+        <>
+            <h1>
+                HOME
+            </h1>
+        <div className={styles.main}>
+            {memes?.length > 0
+                ? memes.map(x => <MemeCard key={x._id} meme={x} />)
+                : <h3 className={styles.noMemes}>No memes yet</h3>
+            }
+        </div>
+        </>
     )
 }
 
