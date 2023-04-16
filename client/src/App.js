88998273e891f3863198eae7b1e1cbc98d06b99a
xcen3k <xcen3k@gmail.com>
2023-04-16 15:57:50 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import { useEffect, useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 import './App.css';
 
-import * as memeService from './services/memeService';
+import { AuthProvider } from './contexts/authContext';
+import { MemeProvider } from './contexts/memeContext';
 import Header from './components/Header/Header';
 // import Details from './components/Details/Details';
-import { AuthProvider } from './contexts/authContext';
 import Login from './components/Login/Login';
 import Register from './components/Register/register';
 import Logout from './components/Logout/Logout';
@@ -14,55 +13,19 @@ import Home from './components/Home/Home';
 import CreateMeme from './components/CreateMeme/CreateMeme';
 
 function App() {
-  const navigate = useNavigate();
-  const [memes, setMemes] = useState([]);
-
-  useEffect(() => {
-    memeService.getAll()
-      .then(result => {
-        setMemes(result);
-      })
-      .catch(error => console.log(error));
-  }, []);
-
-  const updateMemeState = (memeData) => {
-    setMemes(state => [
-        ...state,
-        memeData,
-    ]);
-
-    navigate('/');
-};
-
-  //   useEffect(() => {
-  //     gameService.getAll()
-  //         .then(result => {
-  //             setGames(result);
-  //         });
-  // }, []);
-
-  // useEffect(() => {
-  //   // declare the data fetching function
-  //   const fetchData = async () => {
-  //     const data = await fetch('https://yourapi.com');
-  //   }
-
-  //   // call the function
-  //   fetchData()
-  //     // make sure to catch any error
-  //     .catch(console.error);
-  // }, [])
 
   return (
     <AuthProvider>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home memes={memes} />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/create" element={<CreateMeme updateMemeState={updateMemeState}/>} />
-      </Routes>
+      <MemeProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/create" element={<CreateMeme />} />
+        </Routes>
+      </MemeProvider>
     </AuthProvider >
   );
 }
