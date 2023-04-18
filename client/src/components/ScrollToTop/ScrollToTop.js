523d1a785ef35411157e079f65513783cfa08678
xcen3k <xcen3k@gmail.com>
2023-04-18 22:12:45 +0300

    Create ScrollToTop.js

---
+++ client/src/components/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+import {FaArrowCircleUp} from 'react-icons/fa';
+
+import styles from './ScrollToTop.module.css';
+  
+const ScrollToTop = () =>{
+  
+  const [visible, setVisible] = useState(false)
+  
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300){
+      setVisible(true)
+    } 
+    else if (scrolled <= 300){
+      setVisible(false)
+    }
+  };
+  
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+  
+  window.addEventListener('scroll', toggleVisible);
+  
+  return (
+    <div className={styles.button} >
+     <FaArrowCircleUp onClick={scrollToTop} 
+     style={{display: visible ? 'inline' : 'none'}} />
+    </div>
+  );
+}
+  
+export default ScrollToTop;
