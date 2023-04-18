415888f7fdfc6e45be05549ec716c962443769c5
xcen3k <xcen3k@gmail.com>
2023-04-18 22:12:13 +0300

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,19 +7,19 @@ import { MemeCard } from "../MemeCard/MemeCard";
 
 const Home = () => {
 
-const {memes} = useContext(MemeContext);
+    const { memes } = useContext(MemeContext);
 
     return (
         <section>
-            <h1>
-                ALL MEMES
-            </h1>
-        <div className={styles.main}>
-            {memes?.length > 0
-                ? memes.map(x => <MemeCard key={x._id} meme={x} />)
-                : <h3 className={styles.noMemes}>No memes yet</h3>
-            }
-        </div>
+           <div className={styles.imgContainer}>
+            <img className={styles.img} src="/allMemes.png" alt="" />
+           </div>
+            <div className={styles.main}>
+                {memes?.length > 0
+                    ? memes.map(x => <MemeCard key={x._id} meme={x} />)
+                    : <h3 className={styles.noMemes}>No memes yet</h3>
+                }
+            </div>
         </section>
     )
 }
