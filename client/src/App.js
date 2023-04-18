5f58b80735cd8023d45b1ff8fbb8a4319a011458
xcen3k <xcen3k@gmail.com>
2023-04-18 22:11:37 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import { AuthProvider } from './contexts/authContext';
 import { MemeProvider } from './contexts/memeContext';
+import IsLogged from './components/RouteGuards/IsLogged';
 import Header from './components/Header/Header';
 import Details from './components/Details/Details';
 import Login from './components/Login/Login';
@@ -13,6 +14,8 @@ import Home from './components/Home/Home';
 import CreateMeme from './components/CreateMeme/CreateMeme';
 import EditMeme from './components/EditMeme/EditMeme';
 import DeleteMeme from './components/DeleteMeme/DeleteMeme';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+// import IsAuthor from './components/RouteGuards/IsAuthor';
 
 function App() {
 
@@ -20,15 +23,28 @@ function App() {
     <AuthProvider>
       <MemeProvider>
         <Header />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/create" element={<CreateMeme />} />
+          <Route path="/create" element={
+            <IsLogged>
+              <CreateMeme />
+            </IsLogged>
+          } />
           <Route path="/:memeId" element={<Details />} />
-          <Route path="/:memeId/edit" element={<EditMeme />} />
-          <Route path="/:memeId/delete" element={<DeleteMeme />} />
+          <Route path="/:memeId/edit" element={
+            <IsLogged>
+              <EditMeme />
+            </IsLogged>
+          } />
+          <Route path="/:memeId/delete" element={
+            <IsLogged>
+              <DeleteMeme />
+            </IsLogged>
+          } />
         </Routes>
       </MemeProvider>
     </AuthProvider >
