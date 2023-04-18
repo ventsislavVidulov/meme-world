86a11313232e7e348bd4c932b5e6224a384a620d
xcen3k <xcen3k@gmail.com>
2023-04-18 22:12:02 +0300

    Update EditMeme.js

--- client/src/components/EditMeme/EditMeme.js
+++ client/src/components/EditMeme/EditMeme.js
@@ -12,42 +12,45 @@ const EditMeme = () => {
     const navigate = useNavigate();
     const { editMemeInState } = useContext(MemeContext);
 
+    // console.log(memeId);
+
     useEffect(() => {
         getOne(memeId)
             .then(memeData => {
                 setCurrentMeme(memeData);
             })
-    }, [memeId])
+    }, [memeId]);
 
     const onSubmit = (e) => {
         e.preventDefault();
 
         const memeData = Object.fromEntries(new FormData(e.target));
+        // console.log(memeData);
         editMeme(memeId, memeData)
             .then(result => {
-                editMemeInState(result);
-                // console.log(currentMeme);
+                editMemeInState(memeId, result);
+                // console.log(result);
                 navigate(`/${memeId}`);
             });
     }
-
+    // console.log(currentMeme);
     return (
         <div className={styles.container}>
             <h1>EDIT MEME</h1>
             <form onSubmit={onSubmit}>
                 <div className={styles.inputGroup}>
                     <label htmlFor="title">Title</label>
-                    <input className={styles.rounded} type="text" name="title" id="title" defaultValue={currentMeme.title}/>
+                    <input className={styles.rounded} type="text" name="title" id="title" defaultValue={currentMeme.title} />
                 </div>
                 <div className={styles.inputGroup}>
                     <label htmlFor="imgeUrl">Image url</label>
-                    <input className={styles.rounded} type="text" name="imgUrl" id="imgeUrl" defaultValue={currentMeme.imgUrl}/>
+                    <input className={styles.rounded} type="text" name="imgUrl" id="imgeUrl" defaultValue={currentMeme.imgUrl} />
                 </div>
                 <div className={styles.inputGroup}>
                     <label htmlFor="description">Description</label>
-                    <input className={styles.rounded} type="text" name="description" id="description" defaultValue={currentMeme.description}/>
+                    <input className={styles.rounded} type="text" name="description" id="description" defaultValue={currentMeme.description} />
                 </div>
-                <button type="submit" className={styles.rounded && styles.margin}>
+                <button type="submit" className={styles.rounded && styles.margin && styles.button}>
                     EDIT MEME
                 </button>
             </form>
